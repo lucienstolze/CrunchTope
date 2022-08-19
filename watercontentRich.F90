457ba45b75032135b3d11b44f79c66770f142d89
lucienstolze <lstolze@lbl.gov>
2022-08-19 16:26:39 -0700

    transpiflux print 2

--- watercontentRich.F90
+++ watercontentRich.F90
@@ -130,6 +130,7 @@ DO jz = 1,nz
       pumpterm = -wc(jx,jy,jz)
       END IF
       wc(jx,jy,jz) = wc(jx,jy,jz) + pumpterm    
+  transpiflux(jx,jy,jz) = pumpterm*secyr*dxx(jx)*dyy(jy)*dzz(jx,jy,jz)/dt !m3/yr flux of solutes through transpiration
     ELSEIF (activecellPressure(jx,jy,jz) == 1 .AND. activecellPressure(jx,jy-transpicells,jz) == 0) THEN
         pumpterm = 0.0d0
         pumpterm = pumpterm + dt*transpirate/(secyr*dxx(jx)*dyy(jy)*dzz(jx,jy,jz))
@@ -137,8 +138,9 @@ DO jz = 1,nz
         pumpterm = -wc(jx,jy,jz)
         END IF
         wc(jx,jy,jz) = wc(jx,jy,jz) + pumpterm  
-    ENDIF
   transpiflux(jx,jy,jz) = pumpterm*secyr*dxx(jx)*dyy(jy)*dzz(jx,jy,jz)/dt !m3/yr flux of solutes through transpiration
+    ENDIF
+  
   ENDIF 
   
 
